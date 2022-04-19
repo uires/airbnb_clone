da9274a5ed280fbf227c2c0d9054eb0529301080
Uíres <uiresdeivide1@gmail.com>
2022-04-18 23:25:48 -0300

    fix: Remoção de controlador não utilizado, junto com endpoint não utilizado no controlado do Login

--- back/src/main/java/br/com/airbnb/controller/LoginController.java
+++ back/src/main/java/br/com/airbnb/controller/LoginController.java
@@ -3,10 +3,4 @@ package br.com.airbnb.controller;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class LoginController {
-	/*
-	@GetMapping
-	public String login() {
-		return "login";
-	}*/
-}
+public class LoginController { }
