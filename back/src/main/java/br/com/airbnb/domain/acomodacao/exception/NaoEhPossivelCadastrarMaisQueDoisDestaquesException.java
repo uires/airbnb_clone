5b9c792947659c8ae1a8b0fde6cda031380ff813
uires <uiresdeivide1@gmail.com>
2022-04-27 22:55:29 -0300

    fix: Corrigido http status para exceptions de negócio

--- back/src/main/java/br/com/airbnb/domain/acomodacao/exception/NaoEhPossivelCadastrarMaisQueDoisDestaquesException.java
+++ back/src/main/java/br/com/airbnb/domain/acomodacao/exception/NaoEhPossivelCadastrarMaisQueDoisDestaquesException.java
@@ -1,5 +1,9 @@
 package br.com.airbnb.domain.acomodacao.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class NaoEhPossivelCadastrarMaisQueDoisDestaquesException extends IllegalArgumentException {
 
 	private static final long serialVersionUID = 3809285311915362220L;
