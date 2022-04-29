00eaf1c05757a2e4607511ec190edcbb9efd5781
uires <uiresdeivide1@gmail.com>
2022-04-28 23:42:08 -0300

    fix: Corrigindo modalagem de precificação

--- back/src/main/java/br/com/airbnb/domain/acomodacao/Precificacao.java
+++ back/src/main/java/br/com/airbnb/domain/acomodacao/Precificacao.java
@@ -2,7 +2,6 @@ package br.com.airbnb.domain.acomodacao;
 
 import java.math.BigDecimal;
 
-import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 import br.com.airbnb.domain.acomodacao.exception.PrecoPernoiteNaoPodeSerMenorQue74ReaisException;
@@ -11,22 +10,29 @@ import lombok.NoArgsConstructor;
 
 @Embeddable
 @NoArgsConstructor
-public class PrecoPernoite {
+public class Precificacao {
 
 	@Getter
-	@Column(nullable = false)
 	private BigDecimal valor;
+
+	@Getter
+	private BigDecimal valorMensal;
 
 	// Flag para verificar se permite desconto de 20% para os três primeiros
 	// hóspedes
 	private boolean permiteDescontoPrimeirosTresHospedes;
 
-	public PrecoPernoite(BigDecimal valor, boolean permiteDescontoPrimeirosTresHospides) {
-		if (valor.compareTo(new BigDecimal("74.00")) < 0) {
+	public Precificacao(BigDecimal valor, BigDecimal valorMensal, boolean permiteDescontoPrimeirosTresHospides) {
+		if (valor != null && valorMensal != null) {
+			throw new RuntimeException("Não é possível cadastrar uma acomodação com valor cobrança de noite e mensal");
+		}
+
+		if (valor != null && valor.compareTo(new BigDecimal("74.00")) < 0) {
 			throw new PrecoPernoiteNaoPodeSerMenorQue74ReaisException();
 		}
 
 		this.valor = valor;
+		this.valorMensal = valorMensal;
 		this.permiteDescontoPrimeirosTresHospedes = permiteDescontoPrimeirosTresHospides;
 	}
 
