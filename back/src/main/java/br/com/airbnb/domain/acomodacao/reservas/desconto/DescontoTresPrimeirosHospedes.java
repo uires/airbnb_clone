cbb19b34745f2a08e3eb6d6ab05c1a98e9957b68
uires <uiresdeivide1@gmail.com>
2022-04-29 23:58:47 -0300

    fix: ajuste calculo desconto

--- back/src/main/java/br/com/airbnb/domain/acomodacao/reservas/desconto/DescontoTresPrimeirosHospedes.java
+++ back/src/main/java/br/com/airbnb/domain/acomodacao/reservas/desconto/DescontoTresPrimeirosHospedes.java
@@ -1,6 +1,7 @@
 package br.com.airbnb.domain.acomodacao.reservas.desconto;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import br.com.airbnb.domain.acomodacao.reservas.Reserva;
 
@@ -24,7 +25,8 @@ public class DescontoTresPrimeirosHospedes extends Desconto {
 				&& reserva.getAcomodacao().getReservas().size() <= 2) {
 
 			BigDecimal valorTotal = reserva.getValorTotal();
-			return valorTotal.subtract(valorTotal.multiply(new BigDecimal(this.TAXA_DESCONTO)));
+			return valorTotal.subtract(valorTotal.multiply(new BigDecimal(this.TAXA_DESCONTO))).setScale(2,
+					RoundingMode.HALF_UP);
 		}
 
 		return this.proximo.calcular(reserva);
