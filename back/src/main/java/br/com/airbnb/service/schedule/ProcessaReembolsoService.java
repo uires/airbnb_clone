77d883fa95c0e64e5d68a4186bc786f58fbd9f92
uires <uiresdeivide1@gmail.com>
2022-05-01 23:18:02 -0300

    feat: Adicionado service para envio de e-mail

--- back/src/main/java/br/com/airbnb/service/schedule/ProcessaReembolsoService.java
+++ back/src/main/java/br/com/airbnb/service/schedule/ProcessaReembolsoService.java
@@ -1,7 +1,10 @@
 package br.com.airbnb.service.schedule;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 
+import javax.mail.MessagingException;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +12,9 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import br.com.airbnb.domain.acomodacao.reservas.Reembolso;
+import br.com.airbnb.domain.email.Email;
 import br.com.airbnb.repository.ReembolsoRepository;
+import br.com.airbnb.service.email.EmailService;
 
 @Service
 public class ProcessaReembolsoService {
@@ -17,13 +22,35 @@ public class ProcessaReembolsoService {
 	@Autowired
 	private ReembolsoRepository reembolsoRepository;
 
+	@Autowired
+	private EmailService emailService;
+
 	@Scheduled(cron = "0 0/1 * 1/1 * ?")
 	@Transactional
 	public void processaReembolso() {
 		List<Reembolso> reembolsos = this.reembolsoRepository.findByProcessadoFalse();
 		reembolsos.stream().forEach(reembolso -> {
 			reembolso.processa();
+			try {
+				this.emailService.sendHtmlMessage(this.montaEmailReembolso(reembolso));
+			} catch (MessagingException e) {
+				e.printStackTrace();
+			}
 		});
+	}
+
+	/**
+	 * Monta a estrutura do e-mail (processamento de variáveis do thymeleaf,
+	 * subject, etc)
+	 * 
+	 * @param reembolso
+	 * @return
+	 */
+	private Email montaEmailReembolso(Reembolso reembolso) {
+		HashMap<String, Object> valores = new LinkedHashMap<String, Object>();
+		valores.put("reembolso", reembolso);
+		return new Email("O reembolso da reserva foi realizado com sucesso",
+				reembolso.getReserva().getHospede().getEmail(), "processamento-reembolso.html", valores);
 	}
 
 }
