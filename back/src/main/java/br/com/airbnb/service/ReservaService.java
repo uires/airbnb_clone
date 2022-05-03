396b438c8e702d3f273e61c1cbb3ea611af0017e
uires <uiresdeivide1@gmail.com>
2022-05-02 22:52:07 -0300

    feat: Adicionado método para confirmação de reserva

--- back/src/main/java/br/com/airbnb/service/ReservaService.java
+++ back/src/main/java/br/com/airbnb/service/ReservaService.java
@@ -7,6 +7,7 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.airbnb.controller.exception.EntityNotFoundException;
 import br.com.airbnb.domain.acomodacao.Acomodacao;
 import br.com.airbnb.domain.acomodacao.reservas.Reserva;
 import br.com.airbnb.repository.ReservaRepository;
@@ -23,13 +24,24 @@ public class ReservaService {
 		return reserva;
 	}
 
-	public Optional<Reserva> busca(Long id) {
-		return repository.findById(id);
+	public Reserva busca(Long id) {
+		Optional<Reserva> optional = repository.findById(id);
+		if (!optional.isPresent()) {
+			throw new EntityNotFoundException();
+		}
+
+		return optional.get();
 	}
 
 	@Transactional
 	public Reserva cancelaReserva(Reserva reserva) {
 		reserva.cancela();
+		return reserva;
+	}
+
+	@Transactional
+	public Reserva aprovaReserva(Reserva reserva) {
+		reserva.aprova();
 		return reserva;
 	}
 }
