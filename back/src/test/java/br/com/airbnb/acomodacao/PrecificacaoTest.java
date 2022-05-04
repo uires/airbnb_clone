ce41f53ba44c62564aaf9572e974d004062ea1b5
Uíres <uiresdeivide1@gmail.com>
2022-05-03 23:32:17 -0300

    feat: teste unitário para acomodação e precificação

---
+++ back/src/test/java/br/com/airbnb/acomodacao/PrecificacaoTest.java
@@ -0,0 +1,34 @@
+package br.com.airbnb.acomodacao;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Test;
+
+import br.com.airbnb.domain.acomodacao.Precificacao;
+import br.com.airbnb.domain.acomodacao.exception.PrecoPernoiteNaoPodeSerMenorQue74ReaisException;
+
+public class PrecificacaoTest {
+
+	/**
+	 * Válida a estouro de exception ao criar precificação de acomodação com preço
+	 * menor que R$ 74,00
+	 */
+	@Test
+	public void testaCriacaoAcomodacaoComPrecificacaoMenorIgual74() {
+		assertThrows(PrecoPernoiteNaoPodeSerMenorQue74ReaisException.class,
+				() -> new Precificacao(new BigDecimal("73.99"), null, false));
+
+	}
+
+	/**
+	 * Válida a estouro de exception ao criar precificação de acomodação com preço
+	 * de mês e noite definidos
+	 */
+	@Test
+	public void testaCriacaoAcomodacaoComPrecificacaoMensalENoturnaDefinidas() {
+		assertThrows(RuntimeException.class,
+				() -> new Precificacao(new BigDecimal("500"), new BigDecimal("500"), false));
+	}
+}
