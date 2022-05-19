3249ff482baaa0705a29ccf1b28e41119a7008b7
uires <uiresdeivide1@gmail.com>
2022-05-18 22:57:25 -0300

    feat: Adicionado dados para testes de integração

--- back/src/test/java/br/com/airbnb/integration/repoitory/AcomodacaoServiceTest.java
+++ back/src/test/java/br/com/airbnb/integration/repoitory/AcomodacaoServiceTest.java
@@ -1,28 +1,38 @@
 package br.com.airbnb.integration.repoitory;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Arrays;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 
+import br.com.airbnb.controller.form.ConsultaForm;
 import br.com.airbnb.domain.acomodacao.Acomodacao;
 import br.com.airbnb.domain.acomodacao.Destaque;
 import br.com.airbnb.domain.acomodacao.Hospedes;
 import br.com.airbnb.domain.acomodacao.Precificacao;
 import br.com.airbnb.domain.acomodacao.QuantidadesComodos;
 import br.com.airbnb.domain.acomodacao.TipoLugar;
+import br.com.airbnb.domain.acomodacao.reservas.Reserva;
 import br.com.airbnb.domain.usuario.Usuario;
 import br.com.airbnb.repository.AcomodacaoRepository;
 import br.com.airbnb.repository.UsuarioRepository;
+import br.com.airbnb.service.AcomodacaoService;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestInstance(Lifecycle.PER_CLASS)
 public class AcomodacaoServiceTest {
 
 	@Autowired
@@ -31,13 +41,14 @@ public class AcomodacaoServiceTest {
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 
-	private Acomodacao acomodacao;
+	@Autowired
+	private AcomodacaoService acomodacaoService;
 
 	@Autowired
 	private BCryptPasswordEncoder encoder;
 
 	@BeforeAll
-	private void setAcomodacao() {
+	public void setAcomodacao() {
 		Hospedes hospedes = new Hospedes(2, 2, 0, 0);
 		Precificacao precificacao = new Precificacao(new BigDecimal("500.00"), null, false);
 		QuantidadesComodos quantidadesComodos = new QuantidadesComodos(3, 2);
@@ -45,14 +56,37 @@ public class AcomodacaoServiceTest {
 		String senha = encoder.encode("123456");
 		Usuario usuario = new Usuario(null, "First", "Second", null, "ddd@gmail.com", LocalDate.now(), false, false,
 				senha, null);
-		this.usuarioRepository.save(usuario);
 
-		this.acomodacao = new Acomodacao(TipoLugar.APARTAMENTO, null, hospedes, null, precificacao,
+		Usuario usuarioDois = new Usuario(null, "Second", "First", null, "ccc@gmail.com", LocalDate.now(), false, false,
+				senha, null);
+
+		usuario = this.usuarioRepository.save(usuario);
+		usuarioDois = this.usuarioRepository.save(usuarioDois);
+
+		Acomodacao acomodacao = new Acomodacao(TipoLugar.APARTAMENTO, null, hospedes, null, precificacao,
 				new BigDecimal("75.00"), new BigDecimal("150.00"), "Espaço Juremana é um espaço maravilhoso",
 				Arrays.asList(Destaque.IDEAL_PARA_FAMILIAS), "Espaço Juremana", usuario, LocalTime.NOON, LocalTime.NOON,
 				false, quantidadesComodos, 3);
 
-		this.acomodacaoRepository.save(this.acomodacao);
+		var reservaUm = new Reserva(LocalDateTime.now().plusDays(1L), LocalDateTime.now().plusDays(4L),
+				LocalDateTime.now(), new Hospedes(2, 0, 0, 0), usuarioDois, acomodacao);
+
+		var reservaDois = new Reserva(LocalDateTime.now().plusDays(5L), LocalDateTime.now().plusDays(7L),
+				LocalDateTime.now(), new Hospedes(2, 0, 0, 0), usuarioDois, acomodacao);
+
+		acomodacao.adicionaReserva(reservaUm);
+		acomodacao.adicionaReserva(reservaDois);
+
+		this.acomodacaoRepository.save(acomodacao);
+
+		// Segunda acomodação
+		Hospedes hospedesDois = new Hospedes(1, 2, 0, 0);
+		Acomodacao acomodacaoDois = new Acomodacao(TipoLugar.CASA, null, hospedesDois, null,
+				new Precificacao(new BigDecimal("3550.00"), null, false), new BigDecimal("75.00"),
+				new BigDecimal("55.00"), "Espaço Julinda", Arrays.asList(Destaque.IDEAL_PARA_FAMILIAS),
+				"Espaço Julinda", usuario, LocalTime.NOON, LocalTime.NOON, false, quantidadesComodos, 3);
+
+		this.acomodacaoRepository.save(acomodacaoDois);
 	}
 
 }
