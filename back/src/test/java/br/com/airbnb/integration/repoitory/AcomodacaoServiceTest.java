dc6887a453a1652ba8215600baa6f0b5f40f7868
uires <uiresdeivide1@gmail.com>
2022-05-19 22:48:13 -0300

    feat: Adicionado teste para validar consultar por quantidade criança

--- back/src/test/java/br/com/airbnb/integration/repoitory/AcomodacaoServiceTest.java
+++ back/src/test/java/br/com/airbnb/integration/repoitory/AcomodacaoServiceTest.java
@@ -20,6 +20,7 @@ import org.springframework.test.context.ActiveProfiles;
 import br.com.airbnb.controller.form.ConsultaForm;
 import br.com.airbnb.domain.acomodacao.Acomodacao;
 import br.com.airbnb.domain.acomodacao.Destaque;
+import br.com.airbnb.domain.acomodacao.Espaco;
 import br.com.airbnb.domain.acomodacao.Hospedes;
 import br.com.airbnb.domain.acomodacao.Precificacao;
 import br.com.airbnb.domain.acomodacao.QuantidadesComodos;
@@ -49,7 +50,7 @@ public class AcomodacaoServiceTest {
 
 	@BeforeAll
 	public void setAcomodacao() {
-		Hospedes hospedes = new Hospedes(2, 2, 0, 0);
+		Hospedes hospedes = new Hospedes(2, 1, 0, 0);
 		Precificacao precificacao = new Precificacao(new BigDecimal("500.00"), null, false);
 		QuantidadesComodos quantidadesComodos = new QuantidadesComodos(3, 2);
 
@@ -66,7 +67,7 @@ public class AcomodacaoServiceTest {
 		Acomodacao acomodacao = new Acomodacao(TipoLugar.APARTAMENTO, null, hospedes, null, precificacao,
 				new BigDecimal("75.00"), new BigDecimal("150.00"), "Espaço Juremana é um espaço maravilhoso",
 				Arrays.asList(Destaque.IDEAL_PARA_FAMILIAS), "Espaço Juremana", usuario, LocalTime.NOON, LocalTime.NOON,
-				false, quantidadesComodos, 3);
+				false, quantidadesComodos, 1);
 
 		var reservaUm = new Reserva(LocalDateTime.now().plusDays(1L), LocalDateTime.now().plusDays(4L),
 				LocalDateTime.now(), new Hospedes(2, 0, 0, 0), usuarioDois, acomodacao);
@@ -89,7 +90,7 @@ public class AcomodacaoServiceTest {
 		this.acomodacaoRepository.save(acomodacaoDois);
 
 		// Terceira acomodação
-		Hospedes hospedesTres = new Hospedes(3, 2, 0, 0);
+		Hospedes hospedesTres = new Hospedes(3, 2, 1, 0);
 		Acomodacao acomodacaoTres = new Acomodacao(TipoLugar.CASA, null, hospedesTres,
 				Arrays.asList(Espaco.AREA_DE_JANTAR_EXTERNA, Espaco.JACUZZI),
 				new Precificacao(new BigDecimal("3550.00"), null, false), new BigDecimal("75.00"),
@@ -138,6 +139,14 @@ public class AcomodacaoServiceTest {
 				new BigDecimal("50000.00"), null, null, null, null, null, null);
 		long consultaSumarizadaResultado = this.acomodacaoService.consultaSumarizada(consultaForm);
 		assertEquals(1, consultaSumarizadaResultado);
+	}
+	
+	@Test
+	void testaConsultaSumarizadaPorCriancas() {
+		ConsultaForm consultaForm = new ConsultaForm(null, 2, null, false, null, new BigDecimal("30.00"),
+				new BigDecimal("50000.00"), null, null, null, null, null, null);
+		long consultaSumarizadaResultado = this.acomodacaoService.consultaSumarizada(consultaForm);
+		assertEquals(2, consultaSumarizadaResultado);
 	}
 
 }
