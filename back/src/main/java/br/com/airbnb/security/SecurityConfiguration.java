6a2737d721bfdee53fb0471bff620fec2fc83309
uires <uiresdeivide1@gmail.com>
2022-05-24 20:10:54 -0300

    feat: Adicionado rotas e configuração service autenticação

--- back/src/main/java/br/com/airbnb/security/SecurityConfiguration.java
+++ back/src/main/java/br/com/airbnb/security/SecurityConfiguration.java
@@ -1,37 +1,35 @@
 package br.com.airbnb.security;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import br.com.airbnb.service.security.AutenticacacaoService;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	private DataSource dataSource;
+	private AutenticacacaoService autenticacaoService;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().permitAll().and().csrf().disable();
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/acomodacao/consulta-acomodacao").permitAll()
+				.antMatchers(HttpMethod.POST, "/acomodacao/consulta-sumarizada").permitAll()
+				.antMatchers(HttpMethod.POST, "/cadastro").permitAll().antMatchers(HttpMethod.GET, "/acomodacao/{id}")
+				.permitAll().anyRequest().authenticated().and().csrf().disable();
 	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		BCryptPasswordEncoder cryptPasswordEncoder = new BCryptPasswordEncoder();
-
-		// UserDetails user =
-		// User.builder().username("uires").password(cryptPasswordEncoder.encode("root")).roles("ADM")
-		// .build();
-
-		auth.jdbcAuthentication().dataSource(this.dataSource).passwordEncoder(cryptPasswordEncoder);
+		auth.userDetailsService(this.autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
 	}
 
 	@Bean
