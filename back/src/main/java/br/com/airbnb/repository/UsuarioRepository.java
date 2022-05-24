19885e04413c24d9b6717f8b11b9cb57c7b5cb7a
uires <uiresdeivide1@gmail.com>
2022-05-24 20:10:01 -0300

    feat: Implementando UserDetailsService

--- back/src/main/java/br/com/airbnb/repository/UsuarioRepository.java
+++ back/src/main/java/br/com/airbnb/repository/UsuarioRepository.java
@@ -1,9 +1,13 @@
 package br.com.airbnb.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.airbnb.domain.usuario.Usuario;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{ }
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	Optional<Usuario> findByEmail(String email);
+}
