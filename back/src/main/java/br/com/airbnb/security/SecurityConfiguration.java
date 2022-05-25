ed5deee2691e7342ab5b7084031e204cc784e96c
uires <uiresdeivide1@gmail.com>
2022-05-24 21:07:51 -0300

    feat: Adicionado configuração sessão stateless

--- back/src/main/java/br/com/airbnb/security/SecurityConfiguration.java
+++ back/src/main/java/br/com/airbnb/security/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import br.com.airbnb.service.security.AutenticacacaoService;
@@ -24,7 +25,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers(HttpMethod.POST, "/acomodacao/consulta-acomodacao").permitAll()
 				.antMatchers(HttpMethod.POST, "/acomodacao/consulta-sumarizada").permitAll()
 				.antMatchers(HttpMethod.POST, "/cadastro").permitAll().antMatchers(HttpMethod.GET, "/acomodacao/{id}")
-				.permitAll().anyRequest().authenticated().and().csrf().disable();
+				.permitAll().anyRequest().authenticated().and().csrf().disable().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
 	@Override
