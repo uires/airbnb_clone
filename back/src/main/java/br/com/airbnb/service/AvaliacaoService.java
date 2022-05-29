cde340a43d8d5391b4f7b70ed20a2451c8d2e8a8
uires <uiresdeivide1@gmail.com>
2022-05-28 23:04:17 -0300

    feat: Adicionado regra owner para avaliação de reserva

--- back/src/main/java/br/com/airbnb/service/AvaliacaoService.java
+++ back/src/main/java/br/com/airbnb/service/AvaliacaoService.java
@@ -3,11 +3,14 @@ package br.com.airbnb.service;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import br.com.airbnb.domain.acomodacao.reservas.Reserva;
 import br.com.airbnb.domain.acomodacao.reservas.avaliacao.Avaliacao;
+import br.com.airbnb.domain.usuario.Usuario;
 import br.com.airbnb.repository.AvaliacaoRepository;
+import br.com.airbnb.service.exception.NaoPossuiPermissaoAlterarException;
 
 @Service
 public class AvaliacaoService {
@@ -17,6 +20,12 @@ public class AvaliacaoService {
 
 	@Transactional
 	public Avaliacao cadastra(Reserva reserva, Avaliacao avaliacao) {
+		Usuario usuario = (Usuario) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		
+		if (!usuario.getId().equals(reserva.getHospede().getId())) {
+			throw new NaoPossuiPermissaoAlterarException();
+		}
+		
 		reserva.adicionaAvaliacao(avaliacao);
 		return this.avaliacaoRepository.save(avaliacao);
 	}
