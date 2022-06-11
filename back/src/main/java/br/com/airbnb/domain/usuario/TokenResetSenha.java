29f2dff19f7c4d32cd41f81d2fe69707f00fecf3
uires <uiresdeivide1@gmail.com>
2022-06-10 23:18:33 -0300

    feat: Adicionando entidade para recuperação de senha

---
+++ back/src/main/java/br/com/airbnb/domain/usuario/TokenResetSenha.java
@@ -0,0 +1,43 @@
+package br.com.airbnb.domain.usuario;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@RequiredArgsConstructor
+public class TokenResetSenha {
+
+	@Id
+	@Getter
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Getter
+	@NonNull
+	private String token;
+
+	@NonNull
+	private LocalDateTime dataExpiracao;
+
+	@Getter
+	@NonNull
+	@OneToOne(targetEntity = Usuario.class, fetch = FetchType.EAGER)
+	@JoinColumn(nullable = false, name = "usuario_id")
+	private Usuario usuario;
+
+	public LocalDateTime getDataExpiracao() {
+		return LocalDateTime.from(dataExpiracao);
+	}
+
+}
