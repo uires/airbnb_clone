7a90690334d1c1a47709df7d8b848c48ec14bb95
uires <uiresdeivide1@gmail.com>
2022-06-24 23:23:39 -0300

    fix: Corrigido mapping de pagamento boleto

--- back/src/main/java/br/com/airbnb/domain/acomodacao/reservas/pagamento/PagamentoBoleto.java
+++ back/src/main/java/br/com/airbnb/domain/acomodacao/reservas/pagamento/PagamentoBoleto.java
@@ -2,10 +2,13 @@ package br.com.airbnb.domain.acomodacao.reservas.pagamento;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Entity;
+
 import br.com.airbnb.domain.acomodacao.reservas.Reserva;
 import br.com.airbnb.domain.usuario.pagamento.Cartao;
 import lombok.Getter;
 
+@Entity
 public class PagamentoBoleto extends Pagamento {
 
 	@Getter
