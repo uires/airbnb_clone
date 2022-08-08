7fc7e60d9080202b438619b5fdcea93279514c4e
uires <uiresdeivide1@gmail.com>
2022-08-07 22:10:15 -0300

    feat: Adicionado cenário teste unitário para validar processamento pagamento cartão

--- back/src/test/java/br/com/airbnb/unit/acomodacao/reserva/pagamento/PagamentoTest.java
+++ back/src/test/java/br/com/airbnb/unit/acomodacao/reserva/pagamento/PagamentoTest.java
@@ -1,6 +1,9 @@
 package br.com.airbnb.unit.acomodacao.reserva.pagamento;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -17,6 +20,7 @@ import br.com.airbnb.domain.acomodacao.Hospedes;
 import br.com.airbnb.domain.acomodacao.Precificacao;
 import br.com.airbnb.domain.acomodacao.exception.PagamentoRealizadoException;
 import br.com.airbnb.domain.acomodacao.reservas.Reserva;
+import br.com.airbnb.domain.acomodacao.reservas.pagamento.PagamentoBoleto;
 import br.com.airbnb.domain.acomodacao.reservas.pagamento.PagamentoCartao;
 import br.com.airbnb.domain.acomodacao.reservas.pagamento.TipoPagamento;
 import br.com.airbnb.domain.usuario.Usuario;
@@ -46,8 +50,27 @@ public class PagamentoTest {
 		this.reserva
 				.adicionaPagamento(new PagamentoCartao(null, TipoPagamento.CARTAO_CREDITO, false, null, null, reserva));
 		
+		assertNotNull(this.reserva.getPagamento());
 		assertThrows(PagamentoRealizadoException.class, () -> this.reserva.adicionaPagamento(
 				new PagamentoCartao(null, TipoPagamento.CARTAO_CREDITO, false, null, null, reserva)));
+	}
+
+	@Test
+	public void testPagamentoProcessadoAoAdicionarCartao() {
+		this.reserva
+				.adicionaPagamento(new PagamentoCartao(null, TipoPagamento.CARTAO_CREDITO, false, null, null, reserva));
+
+		assertNotNull(this.reserva.getPagamento());
+		assertTrue(this.reserva.getPagamento().isProcessado());
+	}
+
+	@Test
+	public void testPagamentoNaoProcessadoAoAdicionarPagamentoBoleto() {
+		this.reserva.adicionaPagamento(
+				new PagamentoBoleto(null, TipoPagamento.CARTAO_CREDITO, false, null, null, reserva, ""));
+
+		assertNotNull(this.reserva.getPagamento());
+		assertFalse(this.reserva.getPagamento().isProcessado());
 	}
 
 }
