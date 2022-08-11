7120dd0bfc37343aaed4c74df114940757762f4d
uires <uiresdeivide1@gmail.com>
2022-08-10 22:47:52 -0300

    feat: Ajustado header e finalizando estilo.

--- front/src/app/components/template/header/header.component.ts
+++ front/src/app/components/template/header/header.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  display: boolean = false;
+
   constructor() {}
   ngOnInit(): void {}
 }
--- front/src/app/components/template/header/header.module.ts
+++ front/src/app/components/template/header/header.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { HeaderComponent } from './header.component';
+import { SidebarModule } from 'primeng/sidebar';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SidebarModule],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
